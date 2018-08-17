6cfa860b8b0f16a66cbf1ac8e0ad470c30775fa6
marceloev <marcelo.sinergia@outlook.com>
2018-08-17 00:39:07 -0300

    maintence
    maintence

--- src/main/java/br/com/sinergia/config/security/SecurityConfig.java
+++ src/main/java/br/com/sinergia/config/security/SecurityConfig.java
@@ -18,15 +18,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home", "/login", "*.css").permitAll()
+                .antMatchers("/css/**", "/js/**", "/images/**").permitAll()
+                //.antMatchers("/cadastro**").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
+                .formLogin().loginPage("/login").successForwardUrl("/home").permitAll()
                 .and()
-                .logout()
-                .permitAll();
+                .logout().logoutUrl("login?logout").permitAll()
+                .and()
+                .csrf().disable();
     }
 
     @Bean
